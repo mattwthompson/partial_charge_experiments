63563ec23a87f2c962e88c63a2ce9e0fcd616a90
IAlibay <ialibay@mdanalysis.org>
2024-01-03 16:05:15 +0000

    expand sweep to all ligands

--- scripts/get-conformer.py
+++ scripts/get-conformer.py
@@ -1,3 +1,5 @@
+import pathlib
+
 from rdkit import Chem
 from openff.units import unit
 from openff.toolkit import Molecule
@@ -6,15 +8,19 @@ from openff.toolkit.utils import rdkit_wrapper
 supp = Chem.SDMolSupplier("datafiles/hif2a.sdf", removeHs=False)
 
 rdmols = [mol for mol in supp]
-mol = [Molecule.from_rdkit(m) for m in rdmols if m.GetProp('_Name') == 'lig_338'][0]
-mol._conformers = None
-mol.generate_conformers(
-    n_conformers=1,
-    rms_cutoff=0.25 * unit.angstrom,
-    toolkit_registry=rdkit_wrapper.RDKitToolkitWrapper(),
-)
 
-rdmol = mol.to_rdkit()
+pathlib.Path("./conformers").mkdir(parents=True, exist_ok=True)
 
-with Chem.SDWriter('conformer.sdf') as writer:
-    writer.write(rdmol, confId=0)
+for m in rdmols:
+    mol = Molecule.from_rdkit(m)
+    mol._conformers = None
+    mol.generate_conformers(
+        n_conformers=1,
+        rms_cutoff=0.25 * unit.angstrom,
+        toolkit_registry=rdkit_wrapper.RDKitToolkitWrapper(),
+    )
+
+    rdmol = mol.to_rdkit()
+
+    with Chem.SDWriter(f'conformers/{mol.name}.sdf') as writer:
+        writer.write(rdmol, confId=0)
--- scripts/get-elf1-conformer.py
+++ scripts/get-elf1-conformer.py
@@ -1,3 +1,4 @@
+import pathlib
 from rdkit import Chem
 from openff.units import unit
 from openff.toolkit import Molecule
@@ -6,21 +7,25 @@ from openff.toolkit.utils import rdkit_wrapper
 supp = Chem.SDMolSupplier("datafiles/hif2a.sdf", removeHs=False)
 
 rdmols = [mol for mol in supp]
-mol = [Molecule.from_rdkit(m) for m in rdmols if m.GetProp('_Name') == 'lig_338'][0]
-mol._conformers = None
-mol.generate_conformers(
-    n_conformers=500,
-    rms_cutoff=0.25 * unit.angstrom,
-    toolkit_registry=rdkit_wrapper.RDKitToolkitWrapper(),
-)
 
-mol.apply_elf_conformer_selection(
-    percentage=2,
-    limit=1,
-    rms_tolerance=0.05*unit.angstrom,
-)
+pathlib.Path("./elf1_conformers").mkdir(parents=True, exist_ok=True)
 
-rdmol = mol.to_rdkit()
+for m in rdmols:
+    mol = Molecule.from_rdkit(m)
+    mol._conformers = None
+    mol.generate_conformers(
+        n_conformers=500,
+        rms_cutoff=0.25 * unit.angstrom,
+        toolkit_registry=rdkit_wrapper.RDKitToolkitWrapper(),
+    )
 
-with Chem.SDWriter('conformer.sdf') as writer:
-    writer.write(rdmol, confId=0)
+    mol.apply_elf_conformer_selection(
+        percentage=2,
+        limit=1,
+        rms_tolerance=0.05*unit.angstrom,
+    )
+
+    rdmol = mol.to_rdkit()
+
+    with Chem.SDWriter(f'./elf1_conformers/{mol.name}.sdf') as writer:
+        writer.write(rdmol, confId=0)
