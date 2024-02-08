3976bce17ff8002f82e7f1cc53c57e7f3ff759ec
IAlibay <ialibay@mdanalysis.org>
2024-02-08 23:53:46 +0000

    Add espaloma script

--- scripts/run-espaloma.py
+++ scripts/run-espaloma.py
@@ -1,20 +1,17 @@
 from rdkit import Chem
 from openff.toolkit import Molecule, RDKitToolkitWrapper, AmberToolsToolkitWrapper
-from openff.nagl.toolkits import NAGLRDKitToolkitWrapper
-from openff.toolkit.utils.nagl_wrapper import NAGLToolkitWrapper
 from openff.toolkit.utils.toolkit_registry import toolkit_registry_manager, ToolkitRegistry
+from espaloma_charge.openff_wrapper import EspalomaChargeToolkitWrapper
 
 # Note whether or not you explicitly pass NAGLRDKitToolkitWrapper this issue still occurs
 amber_rdkit = ToolkitRegistry([RDKitToolkitWrapper(), AmberToolsToolkitWrapper()])
 
-supp = Chem.SDMolSupplier("datafiles/CN.sdf", removeHs=False)
-
-m = [Molecule.from_rdkit(i) for i in supp][0]
+m = Molecule.from_smiles('CN')
 
 with toolkit_registry_manager(amber_rdkit):
     print(m.partial_charges)
     m.assign_partial_charges(
-        partial_charge_method="openff-gnn-am1bcc-0.1.0-rc.1.pt",
-        toolkit_registry=NAGLToolkitWrapper(),
+        partial_charge_method="espaloma-am1bcc",
+        toolkit_registry=EspalomaChargeToolkitWrapper(),
     )
     print(m.partial_charges)
